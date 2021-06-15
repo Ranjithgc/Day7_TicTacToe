c8634ddb8cf714582ab9e9035da53c45eca2b0f6
Ranjithgc <ranjith.amca.16@acharya.ac.in>
2021-06-15 17:12:05 +0530

    checked after every move who wins or tie or change the turn

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -89,6 +89,70 @@ public class TicTacToeGame {
 		}
 	}
 
+	/**
+	 * uc7: checked after every move who wins or tie or change the turn
+	 */
+	public static boolean isdraw(char[] board) {
+		int counter = 0;
+		for (int i = 1; i < 10; i++) {
+			if (board[i] != ' ') {
+				counter = counter + 1;
+			}
+		}
+		if (counter == 9) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public static String iswin(char[] board) {
+		String temp = null;
+		for (int choice = 0; choice < 8; choice++) {
+			String line = null;
+
+			switch (choice) {
+			case 0:
+				line = Character.toString(board[1]) + Character.toString(board[2]) + Character.toString(board[3]);
+				break;
+			case 1:
+				line = Character.toString(board[4]) + Character.toString(board[5]) + Character.toString(board[6]);
+				break;
+			case 2:
+				line = Character.toString(board[7]) + Character.toString(board[8]) + Character.toString(board[9]);
+				break;
+			case 3:
+				line = Character.toString(board[1]) + Character.toString(board[4]) + Character.toString(board[7]);
+				break;
+			case 4:
+				line = Character.toString(board[2]) + Character.toString(board[5]) + Character.toString(board[8]);
+				break;
+			case 5:
+				line = Character.toString(board[3]) + Character.toString(board[6]) + Character.toString(board[9]);
+				break;
+			case 6:
+				line = Character.toString(board[1]) + Character.toString(board[5]) + Character.toString(board[9]);
+				break;
+			case 7:
+				line = Character.toString(board[3]) + Character.toString(board[5]) + Character.toString(board[7]);
+				break;
+			}
+			// For X winner
+			if (line.equals("XXX")) {
+				temp = "X";
+				break;
+			}
+			// For O winner
+			else if (line.equals("OOO")) {
+				temp = "O";
+				break;
+			} else {
+				temp = "Play";
+			}
+		}
+		return temp;
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("Welcome to the Tic Tac Toe Game");
 		TicTacToeGame t1 = new TicTacToeGame();
@@ -112,5 +176,29 @@ public class TicTacToeGame {
 		}
 		System.out.println("Current Board:");
 		t1.show();
+		boolean win = false;
+		String result = null;
+		while (win == false) {
+			if (first == 1) {
+				move(player);
+				first = 2;
+			} else {
+				move(player);
+				first = 1;
+			}
+			System.out.println("Current Board:");
+			t1.show();
+			result = iswin(board);
+			if (result == "X") {
+				System.out.println("X is winner");
+				win = true;
+			} else if (result == "O") {
+				System.out.println("O is winner");
+				win = true;
+			} else if (isdraw(board)) {
+				System.out.println("Game is draw");
+				win = true;
+			}
+		}
 	}
 }
