aaf4e28f032c234ac89e3f67619798bf2715c8b1
Ranjithgc <ranjith.amca.16@acharya.ac.in>
2021-06-15 17:00:21 +0530

    allow the palyer to choose a letter X or O

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -17,10 +17,31 @@ public class TicTacToeGame {
 		}
 	}
 
+	/*
+	 * uc2 allow the palyer to choose a letter X or O
+	 */
+	public static char chooseLetter(char userChoose) {
+		char computerChoose = ' ';
+		if (userChoose == 'X' || userChoose == 'x') {
+			computerChoose = 'O';
+		} else if (userChoose == 'O' || userChoose == 'o') {
+			computerChoose = 'X';
+		} else {
+			System.out.println("Invalid character");
+		}
+		return computerChoose;
+	}
+
 	public static void main(String[] args) {
 		System.out.println("Welcome to the Tic Tac Toe Game");
 		TicTacToeGame t1 = new TicTacToeGame();
 		t1.createEmptyBoard();
 		System.out.println("Board is Initialized");
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the letter");
+		char player = sc.next().charAt(0);
+		char computer = chooseLetter(player);
+		System.out.println("player Symbol :" + player);
+		System.out.println("computer symbol: " + computer);
 	}
 }
